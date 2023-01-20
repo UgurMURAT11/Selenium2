52a0771e40ddcfa71a986e740c357c66ef5feaa0
90542 <ugur.murat.08.08@gmail.com>
2023-01-21 00:40:32 +0300

    2. örnek yapıldı

--- src/_01_Print.java
+++ src/_01_Print.java
@@ -3,5 +3,6 @@ public class _01_Print {
     public static void main(String[] args) {
 
         System.out.println("Merhaba dünya");
+        System.out.println("merhaba dünya 2");
             }
 }
